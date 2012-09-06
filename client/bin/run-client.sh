fe7dde3a040d8264427794a96c68a96a0dad3afa
srowen@myrrix.com <srowen@myrrix.com@0d189622-dca1-7e23-9e30-e8815eb7bd2f>
2012-09-06 16:03:42 +0000

    Add utility runner shell scripts, for convenience, to save retype command line arguments
    git-svn-id: http://myrrix-recommender.googlecode.com/svn/trunk@141 0d189622-dca1-7e23-9e30-e8815eb7bd2f

---
+++ client/bin/run-client.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Copyright Myrrix Ltd. Licensed under the Apache License, Version 2.0.
+# See http://myrrix.com/legal
+
+# This is a utility script which may be set with appropriate values, to avoid re-typing them every time
+# on the command line. It illustrates usage as well.
+
+# Set to the hostname or IP address, and port, of the Serving Layer instance to query.
+# Set these when using one Serving Layer. When using multiple partitioned Serving Layers, instead
+# set ALL_PARTITIONS
+HOST=localhost
+PORT=8080
+#ALL_PARTITIONS=foo:80,foo2:8080;bar:8080;baz2:80,baz3:80
+
+# Set if the Serving Layer requires HTTPS
+#SECURE=true
+
+# Set these values if the Serving Layer requires a username and password to access
+#USERNAME=username
+#PASSWORD=password
+
+# Set if translating user IDs
+#TRANSLATE_USER=true
+
+# Set to file containing item IDs if translating item IDs
+#TRANSLATE_ITEM=/path/to/file.txt
+
+# Set any other args here
+#OTHER_ARGS=
+
+# ----- Nothing to set below here -------
+
+if [ -n "${HOST}" ]; then
+  ALL_ARGS="--host=${HOST} --port=${PORT}"
+else
+  ALL_ARGS="--allPartitions=${ALL_PARTITIONS}"
+fi
+
+if [ -n "${SECURE}" ]; then
+  ALL_ARGS="${ALL_ARGS} --secure"
+fi
+
+if [ -n "${USERNAME}" ]; then
+  ALL_ARGS="${ALL_ARGS} --userName=${USERNAME}"
+fi
+if [ -n "${PASSWORD}" ]; then
+  ALL_ARGS="${ALL_ARGS} --password=${PASSWORD}"
+fi
+
+if [ -n "${TRANSLATE_USER}" ]; then
+  ALL_ARGS="${ALL_ARGS} --translateUser"
+fi
+if [ -n "${TRANSLATE_ITEM}" ]; then
+  ALL_ARGS="${ALL_ARGS} --translateItem=${TRANSLATE_ITEM}"
+fi
+
+if [ -n "${OTHER_ARGS}" ]; then
+  ALL_ARGS="${ALL_ARGS} ${OTHER_ARGS}"
+fi
+
+
+CLIENT_JAR=`ls myrrix-client-*.jar`
+
+java -jar ${CLIENT_JAR} ${ALL_ARGS} $@
---
+++ web/bin/run-serving-layer.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# Copyright Myrrix Ltd. Licensed under the Apache License, Version 2.0.
+# See http://myrrix.com/legal
+
+# This is a utility script which may be set with appropriate values, to avoid re-typing them every time
+# on the command line. It illustrates usage as well.
+
+# Set to the location of the input working dir
+LOCAL_INPUT_DIR=/tmp
+
+# Set to the port to listen for HTTP requests. This or SECURE_PORT must be set.
+PORT=8080
+
+# Set to listen for HTTPS connections on the given port. This or PORT must be set.
+#SECURE_PORT=8443
+
+# Set the keystore file to enable SSL / HTTPS, and supply the password if needed.
+#KEYSTORE_FILE=/path/to/keystore
+#KEYSTORE_PASSWORD=password
+
+# Set these values if the Serving Layer requires a username and password to access
+#USERNAME=username
+#PASSWORD=password
+
+# Set to the name of a RescorerProvider if applicable, and the location of a JAR with the class
+#RESCORER=org.example.foo.RescorerProvider
+#RESCORER_JAR=/path/to/jar
+
+# Set any other args here
+#OTHER_ARGS=
+
+# Set any system properties here
+#SYS_PROPS=-Dfoo=bar
+
+# Distribute-mode settings
+
+# Set these to the instance and bucket from which to read a model
+#INSTANCE_ID=123
+#BUCKET=mybucket
+
+# When using multiple partitioned Serving Layers, set ALL_PARTITIONS describing all Serving Layers,
+# and specify which partition this is with PARTITION
+#PARTITION=1
+#ALL_PARTITIONS=foo:80,foo2:8080;bar:8080;baz2:80,baz3:80
+
+# ----- Nothing to set below here -------
+
+ALL_ARGS="--localInputDir=${LOCAL_INPUT_DIR}"
+
+if [ -n "${PORT}" ]; then
+  ALL_ARGS="--port=${PORT}"
+fi
+if [ -n "${SECURE_PORT}" ]; then
+  ALL_ARGS="--securePort=${SECURE_PORT}"
+fi
+
+if [ -n "${KEYSTORE_FILE}" ]; then
+  ALL_ARGS="--keystoreFile=${KEYSTORE_FILE} --keystorePassword=${KEYSTORE_PASSWORD}"
+fi
+
+if [ -n "${USERNAME}" ]; then
+  ALL_ARGS="${ALL_ARGS} --userName=${USERNAME}"
+fi
+if [ -n "${PASSWORD}" ]; then
+  ALL_ARGS="${ALL_ARGS} --password=${PASSWORD}"
+fi
+
+if [ -n "${RESCORER}" ]; then
+  ALL_ARGS="${ALL_ARGS} --rescorerProviderClass=${RESCORER}"
+fi
+ADDITIONAL_CLASSPATH="";
+if [ -n "${RESCORER_JAR}" ]; then
+  ADDITIONAL_CLASSPATH="-cp ${RESCORER_JAR}"
+fi
+
+if [ -n "${INSTANCE_ID}" ]; then
+  ALL_ARGS="${ALL_ARGS} --instanceID=${INSTANCE_ID}"
+fi
+
+if [ -n "${BUCKET}" ]; then
+  ALL_ARGS="${ALL_ARGS} --bucket=${BUCKET}"
+fi
+
+if [ -n "${PARTITION}" ]; then
+  ALL_ARGS="${ALL_ARGS} --partition=${PARTITION}"
+fi
+
+if [ -n "${ALL_PARTITIONS}" ]; then
+  ALL_ARGS="${ALL_ARGS} --allPartitions=${ALL_PARTITIONS}"
+fi
+
+if [ -n "${OTHER_ARGS}" ]; then
+  ALL_ARGS="${ALL_ARGS} ${OTHER_ARGS}"
+fi
+
+
+SERVING_JAR=`ls myrrix-serving-*.jar`
+
+# Set to a value that can be used with the -Xmx flag, like 1200m or 4G or 4g
+HEAP_SIZE=4g
+
+# Unset if on a 32-bit machine
+SIXTYFOUR_BIT_OPTS="-d64 -XX:+UseCompressedOops"
+
+java ${SYS_PROPS} -server -da -dsa -Xmx${HEAP_SIZE} ${SIXTYFOUR_BIT_OPTS}\
+ -XX:NewRatio=12 -XX:+UseParallelGC -XX:+UseParallelOldGC\
+ ${ADDITIONAL_CLASSPATH} -jar ${SERVING_JAR} ${ALL_ARGS} $@
